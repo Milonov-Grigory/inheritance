883ee792e1b9de25e97b0e39fa1a9fb7c214e45a
JonasEnglund <klop9054@gmail.com>
2020-06-07 00:28:12 +0300

    Done

--- parser.cpp
+++ parser.cpp
@@ -1,85 +1,60 @@
 #include <iostream>
 #include <cctype>
+#include "parser.h"
 
-using namespace std;
+bool StrParser::parse(std::istream& stream) {
+    char buffer[30];
+    stream >> buffer;
 
+    for(const char* it = buffer; *it; ++it)
+        if(!(isascii(*it) && !isdigit(*it)))
+            return false;
 
-class Parser
-{
-	public:
-		virtual bool parse( istream& ) = 0;
-};
-
-
-class IntNumParser : public Parser
-{
-	public:
-		bool parse( istream& istr)
-		{
-			char buffer[30]; istr >> buffer;
-
-			for(const char* it = buffer; *it; ++it)
-				if(!isdigit(*it)) return false;
-
-			return true;
-		}
-};
-
-
-class IdParser : public Parser
-{
-	public:
-	bool parse(istream& istr)
-	{
-			char buffer[30]; istr >> buffer;
-			const char * it = buffer;
-
-			if(!isalpha(*it++)) return false;
-
-			for(;*it;++it)
-				if(!isalnum(*it)) return false;
-
-			return true;
-	}
-};
-
-
-//class StrParser....
-// class OpParser - для операций типа + - * / и т.д.
-class CombiParser : public Parser
-{
-	struct Item
-	{
-		Parser* parser;
-		Item* next;
-	};
-	Item* list;
-	public:
-		CombiParser(int num, ...);
-		bool parse( istream& istr)
-		{
-			for(Item* it = list; *it; it = it->next)
-				if(!it->parse(istr)) return false;
-
-			return true;
-		}
-};
-
-
-int main()
-{
-	NumParser num1;
-	NumParser num2;
-	StrParser name;
-	OpParser  op1;
-	OpParser  op2;
-	// Шаблон: name num op num op 
-	CombiParser combi1(5, &name, &num1, &op1, &num2, &op2); 
-
-	cout (combi1.parse(cin) ? "Template match" : "Template not match") << endl;
-
-	return 0;
+    return true;
 }
+
+
+bool IntParser::parse(std::istream &stream) {
+    char buffer[30]; stream >> buffer;
+
+    for(const char* it = buffer; *it; ++it)
+        if(!isdigit(*it)) return false;
+
+    return true;
+
+}
+
+bool IdParser::parse(std::istream &stream) {
+    char buffer[30]; stream >> buffer;
+    const char * it = buffer;
+
+    if(!isalpha(*it++)) return false;
+
+    for(;*it;++it)
+        if(!isalnum(*it)) return false;
+
+    return true;
+
+}
+bool OpParser::parse(std::istream &stream) {
+    char buffer[30]; stream >> buffer;
+
+    for(const char* it = buffer; *it; ++it)
+        if(*it != '+' && *it != '-' && *it != '/' && *it != '*')
+            return false;
+
+    return true;
+}
+
+bool CombiParser::parse(std::istream & stream) {
+    for(Item* it = list; it; it = it->next)
+        if(!it->parser->parse(stream))
+            return false;
+
+    return true;
+
+}
+
 
 
 
